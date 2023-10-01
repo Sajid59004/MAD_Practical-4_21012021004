006ceb65c7a1baf9e10026d97c207b759f5d44c1
Sajid59004 <97504754+Sajid59004@users.noreply.github.com>
2023-10-01 12:17:43 +0530

    MainActivity.kt

--- app/src/main/java/com/example/mad_practical_4_21012021004/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_21012021004/MainActivity.kt
@@ -1,28 +1,23 @@
-package com.example.mad_practical_4_21012021004
-
+package com.example.mad_practocal_4_21012021004
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val login: Button = findViewById(R.id.Login)
-        val signup: Button = findViewById(R.id.signup)
-
-        login.setOnClickListener {
-            Intent(this@MainActivity, login_activity::class.java).also {
-                startActivity(it)
-            }
-        }
-        signup.setOnClickListener{
-            Intent(this@MainActivity,registration_activity::class.java).
-            also{startActivity(it)
-            }
-
-        }
-    }
+ override fun onCreate(savedInstanceState: Bundle?) {
+ super.onCreate(savedInstanceState)
+ setContentView(R.layout.activity_main)
+ val login: Button = findViewById(R.id.Login)
+ val signup: Button = findViewById(R.id.signup)
+ login.setOnClickListener {
+ Intent(this@MainActivity, LoginActivity::class.java).also {
+ startActivity(it)
+ }
+ }
+ signup.setOnClickListener{
+ Intent(this@MainActivity,RegistrationActivity::class.java).
+ also{startActivity(it)
+ }
+ }
+ }
 }
