60e8809f1a2175fa3166d9d26d6e1556f8d6fae2
Sajid59004 <97504754+Sajid59004@users.noreply.github.com>
2023-10-01 12:18:37 +0530

    login_activity.kt

--- app/src/main/java/com/example/mad_practical_4_21012021004/login_activity.kt
+++ app/src/main/java/com/example/mad_practical_4_21012021004/login_activity.kt
@@ -1,21 +1,17 @@
-package com.example.mad_practical_4_21012021004
-
+package com.example.mad_practocal_4_21012021004
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-
-class login_activity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-        val signup: Button = findViewById(R.id.signup)
-
-        signup.setOnClickListener {
-            Intent(this@login_activity, registration_activity::class.java).also {
-                startActivity(it)
-            }
-        }
-    }
+class LoginActivity : AppCompatActivity() {
+ override fun onCreate(savedInstanceState: Bundle?) {
+ super.onCreate(savedInstanceState)
+ setContentView(R.layout.activity_login)
+ val signup: Button = findViewById(R.id.signup)
+ signup.setOnClickListener {
+ Intent(this@LoginActivity, RegistrationActivity::class.java).also {
+ startActivity(it)
+ }
+ }
+ }
 }
